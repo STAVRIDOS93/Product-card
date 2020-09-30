c6654d5ef9094dbb25ddbc7f82eb55fd0b75a4fc
Aleksandr Umanets <a.umanets@ttvk.tui.ua>
2020-09-30 10:49:29 +0300

    start work

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+// import "./style/FONTS/font-tui.css";
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
+);
